e89a4215c4461044553bf6d1f53ee44e2ce1c955
course <raok.sangeetha@gmail.com>
2021-01-25 17:20:01 +0000

    added super user creation function

--- app/app/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from app.calc import add,subtract
-
-
-def CalcTests(TestCase):
-    def test_add_numbers(self):
-        """Tests that two numbers are added together"""
-        self.assertEqual(add(3,8),12)
